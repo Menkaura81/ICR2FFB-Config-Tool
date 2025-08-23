eef63441003a2a3d8f56389a385c0d7d3c09998a
Menkaura81 <bertoalarcon@gmail.com>
2025-08-23 22:39:16 +0200

    bugfixing

--- WinFormsApp1/Properties/Resources.Designer.cs
+++ WinFormsApp1/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ICR2FFB_Config.Properties {
+namespace ICR2FFBConfig.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ICR2FFB_Config.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ICR2FFB_Config.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ICR2FFBConfig.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
